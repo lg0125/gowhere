72f4223b1e23324e3644869dfd316cbeaff3500c
lg0125 <z1737783319@outlook.com>
2020-06-02 19:33:40 +0800

    保持2.3.0版本号不变 与spring cloud 版本号绑定

--- gowhere-pay-alibaba/src/test/java/com/eighty/gowhere/alibaba/GowherePayAlibabaApplicationTests.java
+++ gowhere-pay-alibaba/src/test/java/com/eighty/gowhere/alibaba/GowherePayAlibabaApplicationTests.java
@@ -1,13 +1,20 @@
 package com.eighty.gowhere.alibaba;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.Test;
 
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
-class GowherePayAlibabaApplicationTests {
+public class GowherePayAlibabaApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+        System.out.println("asdasda");
     }
 
 }
