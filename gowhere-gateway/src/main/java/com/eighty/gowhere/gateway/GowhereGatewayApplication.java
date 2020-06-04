3774ae737dfa9bf2aeac0ecc99055b7ddf9e6767
lg0125 <z1737783319@outlook.com>
2020-06-05 01:19:05 +0800

    实现gateway

---
+++ gowhere-gateway/src/main/java/com/eighty/gowhere/gateway/GowhereGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.eighty.gowhere.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GowhereGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GowhereGatewayApplication.class, args);
+    }
+
+}
